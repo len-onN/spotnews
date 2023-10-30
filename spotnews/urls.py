8ad33d8e2611f8a912d525be9b2b2f34d585b5c0
Lenon <lenonphilippi@gmail.com>
2023-10-29 21:14:50 -0300

    [Lenon F. Philippi] - Projeto Spotnews

--- spotnews/urls.py
+++ spotnews/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+# routes
 
 urlpatterns = [
     path("admin/", admin.site.urls),
