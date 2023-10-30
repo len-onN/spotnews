5bba8dcd7b7f26e6d35c3ce21cad7c342a44a8b5
Lenon <lenonphilippi@gmail.com>
2023-10-30 14:35:17 -0300

    req2 - model User - migrate ok

--- news/models.py
+++ news/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 
 class User(models.Model):
     name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
     password = models.CharField(max_length=200)
     role = models.CharField(max_length=200)
 
