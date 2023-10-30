668e233b2a7f32ae835a15829ac60c2f16caf484
Lenon <lenonphilippi@gmail.com>
2023-10-30 15:21:32 -0300

    req3 - Model News

--- news/models.py
+++ news/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from news.validators.validators import validate_title
 
 
 class Category(models.Model):
@@ -18,5 +19,13 @@ class User(models.Model):
         return self.name
 
 
-# class News(models.Model):
-#     pass
+class News(models.Model):
+    title = models.CharField(max_length=200, validators=[validate_title])
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField()
+    image = models.ImageField(upload_to='img/', blank=True, null=True)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
